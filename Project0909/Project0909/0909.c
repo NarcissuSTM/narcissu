20cf38c50267f02b5e203a7fd51df4c7b1eb6f77
songyouhao <s1292851200@163.com>
2022-09-26 22:49:12 +0800

    函数作业

---
+++ Project0909/Project0909/0909.c
@@ -0,0 +1,236 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+//int leap_year(int year)//leap year 闰年
+//{
+//	if ((year % 4 == 0 && year / 100 != 0) || (year % 400 == 0))
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//}
+//
+//int main()
+//{
+//	int year;
+//	int ret = 0;
+//	printf("请输入年份:");
+//	scanf("%d", &year);
+//	ret = leap_year(year);
+//	if (ret == 1)
+//	{
+//		printf("%d年是闰年\n", year);
+//	}
+//	else
+//	{
+//		printf("%d年非闰年\n", year);
+//	}
+//	return 0;
+//}
+
+//函数判断
+
+//int main()
+//{
+//	int year = 0;
+//	printf("输入年份:");
+//	scanf("%d", &year);
+//	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+//	{
+//		printf("yes");
+//	}
+//	else
+//	{
+//		printf("no");
+//	}
+//	return 0;
+//}
+
+//判断
+
+//void print_table(int n)
+//{
+//	int i = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%2d ", j, i, i * j);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print_table(n);
+//	return 0;
+//}
+
+
+
+//
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+
+//void reverse_string(char* str)
+//{
+//	int left = 0;
+//	int right = my_strlen(str) - 1;
+//
+//	while (left < right)
+//	{
+//		char tmp = str[left];
+//		str[left] = str[right];
+//		str[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	reverse_string(arr);
+//
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+
+
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//
+//
+//void reverse_string(char* str)
+//{
+//	int left = 0;
+//	int right = my_strlen(str) - 1;
+//
+//	while (left < right)
+//	{
+//		char tmp = *(str + left);  //与上面等同
+//		*(str + left) = *(str + right);
+//		*(str + right) = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	reverse_string(arr);
+//
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//
+//
+//void reverse_string(char* str)
+//{
+//	char tmp = *str;
+//	int len = my_strlen(str);
+//	*str = *(str + len - 1);
+//	*(str + len - 1) = '\0';
+//
+//	if (my_strlen(str + 1) >= 2)
+//	{
+//		reverse_string(str + 1);
+//	}
+//	*(str + len - 1) = tmp;
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	reverse_string(arr);
+//
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+
+
+//int DigitSum(int n)
+//{
+//	if (n > 9)
+//	{
+//		return DigitSum(n / 10) + n % 10;
+//	}
+//	else
+//	{
+//		return n;
+//	}
+//}
+//
+//int main()
+//{
+//	int num = 1729;
+//	int sum = DigitSum(num);
+//
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+
+
+
+double Pow(int n, int k)
+{
+	if (k == 0)
+	{
+		return 1.0;
+	}
+	else if (k > 0)
+		return n * Pow(n, k - 1);
+	else
+		return 1.0 / (Pow(n, -k));
+}
+
+int main()
+{
+	int n = 0;
+	int k = 0;
+	scanf("%d %d", &n, &k);
+	double ret = Pow(n, k);
+	printf("%lf\n", ret);
+}
